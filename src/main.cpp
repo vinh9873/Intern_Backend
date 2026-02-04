0c13290e0c3f7d6443a4aeb974fc1bf07dfe69c2
vinh9873 <nguyenvinh9837@gmail.com>
2026-02-04 11:16:21 +0700

    Add POST /sum API

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,31 @@
 #include "oatpp/core/macro/component.hpp"
 #include "oatpp/core/macro/codegen.hpp"
 
+#include OATPP_CODEGEN_BEGIN(DTO)
+
+/* ========= DTO ========= */
+
+//Request DTO
+class SumRequestDto : public oatpp::DTO {
+
+  DTO_INIT(SumRequestDto, DTO)
+
+  DTO_FIELD(Int32, a);
+  DTO_FIELD(Int32, b);
+
+};
+
+//Response DTO
+class SumResponseDto : public oatpp::DTO {
+
+  DTO_INIT(SumResponseDto, DTO);
+
+  DTO_FIELD(Int32, result);
+
+};
+
+#include OATPP_CODEGEN_END(DTO)
+
 #include OATPP_CODEGEN_BEGIN(ApiController)
 
 class HelloController : public oatpp::web::server::api::ApiController {
@@ -13,11 +38,22 @@ public:
   HelloController(OATPP_COMPONENT(std::shared_ptr<oatpp::parser::json::mapping::ObjectMapper>, objectMapper))
     : ApiController(objectMapper) {}
 
+  // GET /hello
   ENDPOINT("GET", "/hello", hello) {
     auto res = oatpp::Fields<oatpp::String>::createShared();
     res["message"] = "Hello Intern";
     return createDtoResponse(Status::CODE_200, res);
   }
+
+  //POST /sum
+  ENDPOINT("POST", "/sum", sum, BODY_DTO(oatpp::Object<SumRequestDto>, body)){
+  
+   auto response = SumResponseDto::createShared();
+   response->result = body->a + body->b;
+
+   return createDtoResponse(Status::CODE_200, response);
+  }
+
 };
 
 #include OATPP_CODEGEN_END(ApiController)
